209a48158a8a88c6434e38748c8a7ada9d8e9ada
Michal Turecki <turecki@gmail.com>
2024-06-05 15:44:16 +0100

    File scoped namespaces

--- MarkupConverter/utf16stringreader.cs
+++ MarkupConverter/utf16stringreader.cs
@@ -1,56 +1,48 @@
-﻿namespace MarkupConverter
+﻿namespace MarkupConverter;
+
+internal struct Utf16StringReader(string input)
 {
-    internal struct Utf16StringReader
+    private int currentOffset = 0;
+
+    private static int ConvertToUtf32(char leadingSurrogate, char trailingSurrogate)
     {
-        private int currentOffset;
-        private readonly string input;
+        return (leadingSurrogate - 55296) * 1024 + (trailingSurrogate - 56320) + 65536;
+    }
 
-        public Utf16StringReader(string input)
+    private static bool IsValidUnicodeScalarValue(int codePoint)
+    {
+        if (0 <= codePoint && codePoint <= 55295)
+            return true;
+        if (57344 <= codePoint)
+            return codePoint <= 1114111;
+        return false;
+    }
+
+    public int ReadNextScalarValue()
+    {
+        if(currentOffset >= input.Length)
         {
-            this.input = input;
-            currentOffset = 0;
+            return -1;
         }
-
-        private static int ConvertToUtf32(char leadingSurrogate, char trailingSurrogate)
+        var readInput = input;
+        var readOffset = currentOffset;
+        currentOffset = readOffset + 1;
+        var index = readOffset;
+        var ch1 = readInput[index];
+        var codePoint = (int)ch1;
+        if (char.IsHighSurrogate(ch1) && currentOffset < input.Length)
         {
-            return (leadingSurrogate - 55296) * 1024 + (trailingSurrogate - 56320) + 65536;
-        }
-
-        private static bool IsValidUnicodeScalarValue(int codePoint)
-        {
-            if (0 <= codePoint && codePoint <= 55295)
-                return true;
-            if (57344 <= codePoint)
-                return codePoint <= 1114111;
-            return false;
-        }
-
-        public int ReadNextScalarValue()
-        {
-            if(currentOffset >= input.Length)
+            var ch2 = input[currentOffset];
+            if (char.IsLowSurrogate(ch2))
             {
-                return -1;
+                currentOffset = currentOffset + 1;
+                codePoint = ConvertToUtf32(ch1, ch2);
             }
-            var readInput = input;
-            var readOffset = currentOffset;
-            currentOffset = readOffset + 1;
-            var index = readOffset;
-            var ch1 = readInput[index];
-            var codePoint = (int)ch1;
-            if (char.IsHighSurrogate(ch1) && currentOffset < input.Length)
-            {
-                var ch2 = input[currentOffset];
-                if (char.IsLowSurrogate(ch2))
-                {
-                    currentOffset = currentOffset + 1;
-                    codePoint = ConvertToUtf32(ch1, ch2);
-                }
-            }
-            if (IsValidUnicodeScalarValue(codePoint))
-            {
-                return codePoint;
-            }
-            return 65533;
         }
+        if (IsValidUnicodeScalarValue(codePoint))
+        {
+            return codePoint;
+        }
+        return 65533;
     }
 }
